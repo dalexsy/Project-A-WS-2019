535a5814aae16326b7daa4d482a61359e8d13e54
Daryl <daryl.alexsy@gmail.com>
2019-10-17 20:16:50 +0200

    [added] generic collision detection script

---
+++ Assets/Daryl/Scripts/CollisionDetection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    [SerializeField] string targetTag = null;
+
+    private bool isCollidingWithTarget = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == targetTag)
+            this.isCollidingWithTarget = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == targetTag)
+            this.isCollidingWithTarget = false;
+    }
+}
