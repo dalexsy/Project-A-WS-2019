363609af19c53833dd1c711099114e5aabb20131
Daryl <daryl.alexsy@gmail.com>
2019-12-12 14:47:22 +0100

    actually fixed mouse input

--- Assets/Master/Scripts/Player/PlayerMovement.cs
+++ Assets/Master/Scripts/Player/PlayerMovement.cs
@@ -104,7 +104,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void MouseInput()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
