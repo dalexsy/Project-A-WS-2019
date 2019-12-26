0b811e88653626622ceb5e807f2d78791be86c62
Daryl <daryl.alexsy@gmail.com>
2019-12-26 21:07:14 +0100

    fixed swiping bools for mouse

--- Assets/Master/Scripts/Misc/InputManager.cs
+++ Assets/Master/Scripts/Misc/InputManager.cs
@@ -21,7 +21,37 @@ public class InputManager : MonoBehaviour
 
     private void Update()
     {
-        DetectSwipe();
+        if (isUsingTouch) DetectSwipe();
+        else DetectMouse();
+    }
+
+    private void DetectMouse()
+    {
+        var startTime = Time.time;
+
+        // Left click swipe
+        if (Input.GetMouseButtonDown(0)) startTime = Time.time;
+
+        if (Input.GetMouseButton(0))
+        {
+            var timeElapsed = Time.time - startTime;
+            if (timeElapsed > .5) isSwiping = true;
+            else isSwiping = false;
+        }
+
+        if (Input.GetMouseButtonUp(0)) isSwiping = false;
+
+        // Right click swipe
+        if (Input.GetMouseButtonDown(1)) startTime = Time.time;
+
+        if (Input.GetMouseButton(1))
+        {
+            var timeElapsed = Time.time - startTime;
+            if (timeElapsed > .5) isDoubleSwiping = true;
+            else isDoubleSwiping = false;
+        }
+
+        if (Input.GetMouseButtonUp(1)) isDoubleSwiping = false;
     }
 
     private void DetectSwipe()
