8296ae2e8ceaa2c2f696bdef47148a7f77e55bd4
Daryl <daryl.alexsy@gmail.com>
2020-01-04 18:31:43 +0100

    aligned waypoint selection to view

--- Assets/Master/Scripts/VFX/UniversalVFXManager.cs
+++ Assets/Master/Scripts/VFX/UniversalVFXManager.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 
+
 public class UniversalVFXManager : MonoBehaviour
 {
     public void PlayVFX(Transform vfxSource, GameObject particlePrefab)
     {
-        GameObject particle = Instantiate(particlePrefab, vfxSource.position, Quaternion.identity);
+        GameObject particle = Instantiate(particlePrefab,
+                                          vfxSource.position,
+                                          vfxSource.transform.rotation * particlePrefab.transform.rotation);
+
+        particle.transform.Translate(0, 0.06f, 0);
+
         Destroy(particle, particlePrefab.GetComponent<ParticleSystem>().main.duration);
     }
 }
